40e6c82326fdb76e1f40b62a04739c6930c56a0f
peng yongjia <normaluser@DESKTOP-Q4S4F18.>
2025-05-10 22:18:43 +0200

    ft_striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,34 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yopeng <yopeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/08 15:07:17 by yopeng            #+#    #+#             */
-/*   Updated: 2025/05/08 15:07:17 by yopeng           ###   ########.fr       */
+/*   Created: 2025/05/09 18:05:26 by yopeng            #+#    #+#             */
+/*   Updated: 2025/05/09 18:05:26 by yopeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	char			*str;
 	unsigned int	i;
 
 	if (!s || !f)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (!str)
-		return (NULL);
+		return ;
 	i = 0;
 	while (s[i])
 	{
-		str[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
 }
